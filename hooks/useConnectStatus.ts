a83eb92c3f7eadc59f3d0a69fba4ea520a3a1d3d
linyupark <linyupark@gmail.com>
2020-03-16 17:24:30 +0800

    +add useConnectStatus hooks

---
+++ hooks/useConnectStatus.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+
+export default function useConnectStatus(ws: WebSocket) {
+  const [readyState, setReadyState] = useState(ws.readyState);
+  const [stateName, setStateName] = useState('');
+
+  useEffect(() => {
+
+    function handleOnOpen() {
+      setReadyState(ws.readyState);
+    }
+
+    function handleOnClose() {
+      setReadyState(ws.readyState);
+    }
+
+    ws.addEventListener('open', handleOnOpen);
+    ws.addEventListener('close', handleOnClose);
+
+    console.log('ws state event listener added');
+
+    return () => {
+      ws.removeEventListener('open', handleOnOpen);
+      ws.removeEventListener('close', handleOnClose);
+      console.log('ws state event listener removed');
+    }
+
+  }, []);
+
+  useEffect(() => {
+    setStateName(['连接中', '已连接', '断开中', '已断开'][readyState]);
+  }, [readyState]);
+
+  return stateName;
+}
